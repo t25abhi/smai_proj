586be72a844eba8fe04455ef531270522229c2fa
t25abhi <38892109+t25abhi@users.noreply.github.com>
2018-05-04 00:25:16 +0530

    Add files via upload

---
+++ mnist_mse_CC_noise.py
@@ -0,0 +1,117 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Project 37 SMAI Spring 2018
+
+@author: abhishek
+"""
+
+from __future__ import print_function
+import keras
+from keras.datasets import mnist
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten
+from keras.layers import Conv2D, MaxPooling2D
+from keras import backend as K
+
+import matplotlib.pyplot as plt
+
+batch_size = 128
+num_classes = 10
+epochs = 150
+
+# input image dimensions
+img_rows, img_cols = 28, 28
+
+
+# the data, split between train and test sets
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+if K.image_data_format() == 'channels_first':
+    x_train = x_train.reshape(x_train.shape[0], 1, img_rows, img_cols)
+    x_test = x_test.reshape(x_test.shape[0], 1, img_rows, img_cols)
+    input_shape = (1, img_rows, img_cols)
+else:
+    x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, 1)
+    x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 1)
+    input_shape = (img_rows, img_cols, 1)
+
+x_train = x_train.astype('float32')
+x_test = x_test.astype('float32')
+x_train /= 255
+x_test /= 255
+print('x_train shape:', x_train.shape)
+print(x_train.shape[0], 'train samples')
+print(x_test.shape[0], 'test samples')
+
+
+from random import sample, random, seed
+import copy
+
+seed(2)
+
+yy_train = copy.deepcopy(y_train)
+
+# examples_to_corrupt
+eta_list = [0.4, 0.43, 0.45, 0.48, 0.5, 0.52, 0.55,  0.57, 0.58, 0.6]
+
+for i in range(len(y_train)):
+    old_index = y_train[i]
+    eta = eta_list[old_index]
+    if random() < eta:
+        all_labels = range(10)
+        all_labels.remove(old_index)
+        new_index = sample(all_labels, k=1)
+        y_train[i] = new_index[0]        
+
+print ((y_train == yy_train).sum())
+
+# convert class vectors to binary class matrices
+y_train = keras.utils.to_categorical(y_train, num_classes)
+y_test = keras.utils.to_categorical(y_test, num_classes)
+
+model = Sequential()
+model.add(Conv2D(32, kernel_size=(3, 3),
+                 activation='relu',
+                 input_shape=input_shape))
+model.add(Conv2D(64, (3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+model.add(Flatten())
+model.add(Dense(128, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(num_classes, activation='softmax'))
+
+model.compile(loss=keras.losses.mean_squared_error,
+              optimizer='sgd',
+              metrics=['accuracy'])
+print(model.summary())
+
+history = model.fit(x_train, y_train,
+              batch_size=batch_size,
+              epochs=epochs,
+              verbose=1, shuffle=True,
+ 
+#-- summarize history for accuracy
+plt.plot(history.history['acc'])
+plt.plot(history.history['val_acc'])
+plt.title('model accuracy')
+plt.ylabel('accuracy')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper left')
+plt.show()
+
+#-- summarize history for loss
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.title('model loss')
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper left')
+plt.show()
+
+score = model.evaluate(x_test, y_test, verbose=0)
+
+print('Test loss:', score[0])
+print('Test accuracy:', score[1])
+
